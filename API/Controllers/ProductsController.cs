1b99d94308dd77268058453b41f047b8e7077d0b
vijay22mali <vijay22mali@gmail.com>
2023-06-23 17:33:08 +0530

    End of section3

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 
 
 using Core.Entities;
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -13,23 +14,33 @@ namespace API.Controllers
     public class ProductsController : ControllerBase
     {
         private readonly ILogger<ProductsController> _logger;
-        private readonly StoreContext _context;
-        public ProductsController(ILogger<ProductsController> logger,StoreContext context)
+        private readonly IProductRepository _repo;
+        public ProductsController(ILogger<ProductsController> logger,IProductRepository repo)
         {
             _logger = logger;
-            _context=context;
+           _repo=repo;
         }
 
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts(){
-             var products=await _context.Products.ToListAsync();
-             return products;
+             var products=await _repo.GetProductsAsync();
+             return Ok(products);
         }
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GtProduct(int id)
         {
-            return await _context.Products.FindAsync(id);
+            return await _repo.GetProductByIdAsync(id);
             
+        }
+        [HttpGet("brands")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
+        {
+            return Ok(await _repo.GetProductBrandsAsync());
+        }
+        [HttpGet("types")]
+        public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
+        {
+            return Ok(await _repo.GetProductTypesAsync());
         }
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,6 +14,9 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<StoreContext>(opt=>{
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddScoped<IProductRepository,ProductRepository>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -28,4 +32,16 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using var scope=app.Services.CreateScope();
+var services=scope.ServiceProvider;
+var context=services.GetRequiredService<StoreContext>();
+var logger=services.GetRequiredService<ILogger<Program>>();
+try
+{
+    await context.Database.MigrateAsync();
+    await StoreContextSeed.SeedAsnyc(context);
+}
+catch(Exception ex){
+    logger.LogError(ex,"An error ocured during migration");
+}
 app.Run();
