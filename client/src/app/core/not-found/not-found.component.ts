848d589f323229c65c2b4eb23ca4cbe1030b8f21
vijay22mali <vijay22mali@gmail.com>
2023-07-01 18:32:21 +0530

    End of Section-11

---
+++ client/src/app/core/not-found/not-found.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-not-found',
+  templateUrl: './not-found.component.html',
+  styleUrls: ['./not-found.component.scss']
+})
+export class NotFoundComponent {
+  error:any;
+  
+  constructor(private router:Router){
+    const navigation=this.router.getCurrentNavigation();
+    this.error=navigation?.extras?.state?.['error'];
+  }
+}
